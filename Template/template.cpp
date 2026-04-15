dcf033b7155f95a042eefcc8da484138d7b0c049
goldenpotato <okina.etsushi@gmail.com>
2026-04-15 19:14:03 +0900

    update

---
+++ Template/template.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using ld=long double;
+#define all(a)a.begin(),a.end()
+#define rall(a)a.rbegin(),a.rend()
+#define rep(i,n)for(int i=0;i<(n);++i)
+#define YN(ans)cout<<(ans?"Yes":"No")<<'\n'
+constexpr int inf=2e9;
+constexpr ll INF=2e18;
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    
+}
