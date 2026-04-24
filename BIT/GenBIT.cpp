39f231ac2a45c6b943caa293814f23657aea6a82
goldenpotato <okina.etsushi@gmail.com>
2026-04-24 15:44:15 +0900

    fix GenBIT

--- BIT/GenBIT.cpp
+++ BIT/GenBIT.cpp
@@ -4,13 +4,13 @@ struct GenBIT{
     using T=typename G::T;
     int n;
     vector<T>bit;
-    GenBIT(int n_):n(n_+1),bit(n,0){}
-    GenBIT(const vector<T>&v):n(v.size()+1),bit(n,0){
+    GenBIT(int n_):n(n_+1),bit(n,G::e()){}
+    GenBIT(const vector<T>&v):n(v.size()+1),bit(n,G::e()){
         rep(i,n-1)add(i,v[i]);
     }void add(int i,T x){
         for(++i;i<n;i+=i&-i)bit[i]=G::op(bit[i],x);
     }T sum(int i){
-        T s=0;
+        T s=G::e();
         for(++i;i>0;i-=i&-i)s=G::op(s,bit[i]);
         return s;
     }T query(int l,int r){
