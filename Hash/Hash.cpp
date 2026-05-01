d56de17e7749bd1edfef0c0fc584db2f47fc08c4
goldenpotato <okina.etsushi@gmail.com>
2026-05-01 09:14:17 +0900

    fix Hash and prime

--- Hash/Hash.cpp
+++ Hash/Hash.cpp
@@ -5,14 +5,12 @@ struct Hash{
         x=(x^x>>30)*0xbf58476d1ce4e5b9;
         x=(x^x>>27)*0x94d049bb133111eb;
         return x^x>>31;
+    }static ull rng(){
+        static const ull FIXED_RANDOM=chrono::steady_clock::now().time_since_epoch().count();
+        return FIXED_RANDOM;
     }ull operator()(ull x)const{
-        static const ull FIXED_RANDOM=chrono::steady_clock::now().time_since_epoch().count();
-        return hash(x+FIXED_RANDOM);
-    }
-};
-struct PairHash{
-    ull operator()(const pair<ll,ll>&p)const{
-        static const ull FIXED_RANDOM=chrono::steady_clock::now().time_since_epoch().count();
-        return Hash::hash(Hash::hash((ull)p.first+FIXED_RANDOM)^Hash::hash((ull)p.second+FIXED_RANDOM)<<1);
+        return hash(x+rng());
+    }ull operator()(const pair<ll,ll>&p)const{
+        return hash(hash((ull)p.first+rng())^hash((ull)p.second+rng())<<1);
     }
 };
--- Math/prime/PrimeSieve.cpp
+++ Math/prime/PrimeSieve.cpp
@@ -1,6 +1,6 @@
 // PrimeSieve
 vector<bool>PrimeSieve(int n){
     vector<bool>v(n+1);
-    for(int i=2;i<=n;++i)if(!v[i])for(int j=i+i;j<=n;++j)v[j]=1;
+    for(int i=2;i<=n;++i)if(!v[i])for(int j=i+i;j<=n;j+=i)v[j]=1;
     return v;
 }
