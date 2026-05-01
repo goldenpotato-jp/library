392079bc158f294c7632454bcd0720b03da8893c
goldenpotato <okina.etsushi@gmail.com>
2026-05-01 09:21:45 +0900

    fix FactorSieve

--- Math/prime/FactorSieve.cpp
+++ Math/prime/FactorSieve.cpp
@@ -1,6 +1,6 @@
 // FactorSieve
 vector<ll>FactorSieve(int n){
     vector<ll>v(n+1);
-    for(int i=2;i<=n;++i)if(!v[i])for(int j=i;j<=nl;j+=i)v[j]=i;
+    for(int i=2;i<=n;++i)if(!v[i])for(int j=i;j<=n;j+=i)v[j]=i;
     return v;
 }
