09ddca109383915ac7eb2f918dc604eefb204354
goldenpotato <okina.etsushi@gmail.com>
2026-05-10 07:24:47 +0900

    update template.cpp for precompile

--- Template/template.cpp
+++ Template/template.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include"bits/stdc++.h"
 using namespace std;
 using ll=long long;
 using ull=unsigned long long;
@@ -13,5 +13,5 @@ constexpr ll INF=2e18;
 int main(){
     cin.tie(0)->sync_with_stdio(0);
     cout<<fixed<<setprecision(0);
-    
+
 }
