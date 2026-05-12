efb95dc01670fa482c4553f987c467cfa9bcdb78
goldenpotato <okina.etsushi@gmail.com>
2026-05-12 19:55:40 +0900

    O(N)built Combination

--- Math/Combination.cpp
+++ Math/Combination.cpp
@@ -1,20 +1,12 @@
 // Combination
 struct Combination{
     ll mod;
-    vector<ll>fact,fact_inv;
-    Combination(int n,ll mod_):mod(mod_),fact(n+1),fact_inv(n+1){
-        fact[0]=1,fact_inv[0]=1;
-        for(int i=1;i<=n;++i){
-            fact[i]=fact[i-1]*i%mod;
-            fact_inv[i]=inv(fact[i]);
-        }
-    }ll inv(ll x){
-        ll n=mod-2,r=1;
-        while(n){
-            if(n&1)r=r*x%mod;
-            x=x*x%mod;
-            n>>=1;
-        }return r;
+    vector<ll>fact,inv,fact_inv;
+    Combination(int n,ll mod_=998244353):mod(mod_),fact(n+1),inv(n+1),fact_inv(n+1){
+        fact[0]=inv[1]=fact_inv[0]=1;
+        for(int i=1;i<=n;++i)fact[i]=fact[i-1]*i%mod;
+        for(int i=2;i<=n;++i)inv[i]=mod-mod/i*inv[mod%i]%mod;
+        for(int i=1;i<=n;++i)fact_inv[i]=fact_inv[i-1]*inv[i]%mod;
     }ll get(int n,int r){
         return fact[n]*fact_inv[r]%mod*fact_inv[n-r]%mod;
     }
