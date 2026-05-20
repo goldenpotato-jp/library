69910c4a9785077ac394ba44e3fb4b9115182b4a
goldenpotato <okina.etsushi@gmail.com>
2026-05-20 12:46:33 +0900

    update Combination

--- Math/Combination.cpp
+++ Math/Combination.cpp
@@ -8,6 +8,7 @@ struct Combination{
         for(int i=2;i<=n;++i)inv[i]=mod-mod/i*inv[mod%i]%mod;
         for(int i=1;i<=n;++i)fact_inv[i]=fact_inv[i-1]*inv[i]%mod;
     }ll operator()(int n,int r)const{
+        if(n<r||n<0||r<0)return 0;
         return fact[n]*fact_inv[r]%mod*fact_inv[n-r]%mod;
     }
 };
