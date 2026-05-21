b233075ad98f72c75db75f575818ce4600f50d78
goldenpotato <okina.etsushi@gmail.com>
2026-05-21 17:23:12 +0900

    add Mo

---
+++ Mo/Mo.cpp
@@ -0,0 +1,28 @@
+// Mo
+struct Mo{
+    int width;
+    vector<int>left,right,order;
+    Mo(int N,int Q):order(Q){
+        width=max<int>(1,1.*N/max<ld>(1.,sqrt(Q/1.5)));
+        iota(all(order),0);
+    }void insert(int l, int r){
+        left.emplace_back(l);
+        right.emplace_back(r+1);
+    }template<typename AL,typename AR,typename DL,typename DR,typename REM>
+    void run(const AL&add_left,const AR&add_right,const DL&delete_left,const DR&delete_right,const REM&rem){
+        sort(all(order),[&](int a,int b){
+            int ablock=left[a]/width,bblock=left[b]/width;
+            if(ablock!=bblock)return ablock<bblock;
+            if(ablock&1)return right[a]<right[b];
+            return right[a]>right[b];
+        });
+        int nl=0,nr=0;
+        for(auto idx:order){
+            while(nl>left[idx])add_left(--nl);
+            while(nr<right[idx])add_right(nr++);
+            while(nl<left[idx])delete_left(nl++);
+            while(nr>right[idx])delete_right(--nr);
+            rem(idx);
+        }
+    }
+};
