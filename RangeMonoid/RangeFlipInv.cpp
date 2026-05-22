588bf7c9335c03d8f6bda3cbe8598e8549125f79
goldenpotato <okina.etsushi@gmail.com>
2026-05-22 15:35:04 +0900

    update RangeFlipInv

--- RangeMonoid/RangeFlipInv.cpp
+++ RangeMonoid/RangeFlipInv.cpp
@@ -1,15 +1,15 @@
 // RangeFlipInv
 struct RangeFlipInv{
-    using S=tuple<ll,ll,ll>;
+    using S=tuple<int,int,ll>;
     using F=bool;
     static S e(){
         return{0,0,0};
     }static F id(){
         return 0;
     }static S op(S a,S b){
-        return{get<0>(a)+get<0>(b),get<1>(a)+get<1>(b),get<2>(a)+get<2>(b)+get<1>(a)*get<0>(b)};
+        return{get<0>(a)+get<0>(b),get<1>(a)+get<1>(b),get<2>(a)+get<2>(b)+(ll)get<1>(a)*get<0>(b)};
     }static S mapping(F f,S x){
-        return(f?S{get<1>(x),get<0>(x),get<0>(x)*get<1>(x)-get<2>(x)}:x);
+        return(f?S{get<1>(x),get<0>(x),(ll)get<0>(x)*get<1>(x)-get<2>(x)}:x);
     }static F composition(F f,F g){
         return f^g;
     }
