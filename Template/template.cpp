61f3283ffe056cde3800fb25773e59e75ac26450
goldenpotato <okina.etsushi@gmail.com>
2026-06-03 17:28:47 +0900

    add chmax chmin

--- Template/template.cpp
+++ Template/template.cpp
@@ -8,6 +8,8 @@ using ld=long double;
 #define rep(i,n)for(int i=0;i<(n);++i)
 #define YN(ans)cout<<(ans?"Yes\n":"No\n")
 #define uni(a)sort(all(a)),a.erase(unique(all(a)),a.end())
+template<typename T>void chmin(T&a,T b){if(a>b)a=b;}
+template<typename T>void chmax(T&a,T b){if(a<b)a=b;}
 constexpr int inf=2e9;
 constexpr ll INF=2e18;
 
