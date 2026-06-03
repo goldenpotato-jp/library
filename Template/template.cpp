0846bef31063752e5815793f68f6e3f292a9767e
goldenpotato <okina.etsushi@gmail.com>
2026-06-03 13:19:04 +0900

    update template

--- Template/template.cpp
+++ Template/template.cpp
@@ -7,6 +7,7 @@ using ld=long double;
 #define rall(a)a.rbegin(),a.rend()
 #define rep(i,n)for(int i=0;i<(n);++i)
 #define YN(ans)cout<<(ans?"Yes\n":"No\n")
+#define uni(a)sort(all(a)),a.erase(unique(all(a)),a.end())
 constexpr int inf=2e9;
 constexpr ll INF=2e18;
 
