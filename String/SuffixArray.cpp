1bc650ef49152d95f118523597971562d5ef6e0a
goldenpotato <okina.etsushi@gmail.com>
2026-07-01 11:47:34 +0900

    add LCPArray

--- String/SuffixArray.cpp
+++ String/SuffixArray.cpp
@@ -12,10 +12,6 @@ struct SuffixArray{
         int n=0;
         rep(i,s.size())n+=i>0&&s[idx[i-1]]!=s[idx[i]],v[idx[i]]=n;
         sa=sa_is(v,n);
-    }int operator[](int i){
-        return sa[i];
-    }int size(){
-        return sa.size();
     }vector<int>sa_is(const vector<int>&s,int n){
         if(s.empty())return{};
         if(s.size()==1)return{0};
@@ -43,7 +39,7 @@ struct SuffixArray{
         rep(i,s.size()-1)if(!ls[i]&&ls[i+1])lms.emplace_back(i+1);
         induce(lms);
         if(m){
-            vector<int>sorted_lms,s2(m);
+            vector<int>sorted_lms,s2(m),sa2;
             sorted_lms.reserve(m);
             for(int e:res)if(lms_map[e]>-1)sorted_lms.emplace_back(e);
             int n2=0;
@@ -56,9 +52,15 @@ struct SuffixArray{
                     if(l==s.size()||s[l]!=s[r])diff=1;
                 }else diff=1;
                 n2+=diff,s2[lms_map[sorted_lms[i+1]]]=n2;
-            }auto sa2=sa_is(s2,n2);
+            }sa2=sa_is(s2,n2);
             rep(i,m)sorted_lms[i]=lms[sa2[i]];
             induce(sorted_lms);
         }return res;
+    }vector<int>get(){
+        return sa;
+    }int operator[](int i){
+        return sa[i];
+    }int size(){
+        return sa.size();
     }
 };
