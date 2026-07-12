6c2079c7853e41c3961ac730df6b952c0184d68b
goldenpotato <okina.etsushi@gmail.com>
2026-07-12 17:52:11 +0900

    add ZAlgorithm

---
+++ String/ZAlgorithm.cpp
@@ -0,0 +1,26 @@
+// ZAlgorithm
+struct ZAlgorithm{
+    vector<int>z;
+    ZAlgorithm(const string&s):z(s.size()){
+        z[0]=s.size();
+        int l=0,r=0;
+        for(int i=1;i<s.size();++i){
+            if(z[i-l]>=r-i){
+                chmax(r,i);
+                while(r<s.size()&&s[r]==s[r-i])++r;
+                z[i]=r-i,l=i;
+            }else z[i]=z[i-l];
+        }
+    }template<class T>
+    ZAlgorithm(const vector<T>&v):z(v.size()){
+        z[0]=v.size();
+        int l=0,r=0;
+        for(int i=1;i<v.size();++i){
+            if(z[i-l]>=r-i){
+                chmax(r,i);
+                while(r<v.size()&&v[r]==v[r-i])++r;
+                z[i]=r-i,l=i;
+            }else z[i]=z[i-l];
+        }
+    }
+};
